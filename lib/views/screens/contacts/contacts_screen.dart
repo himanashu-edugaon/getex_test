7e33dc2e56097215429f9d086980da0896a8f79e
himanashu-edugaon <155522579+himanashu-edugaon@users.noreply.github.com>
2024-03-14 15:49:04 +0530

    when i allow the contact than that time show contact that are solve error

--- lib/views/screens/contacts/contacts_screen.dart
+++ lib/views/screens/contacts/contacts_screen.dart
@@ -41,9 +41,13 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void getContactPermission() async {
     if (await Permission.contacts.isGranted) {
-      fetchContacts();
+      fetchContacts(); // Fetch contacts if permission is already granted
     } else {
-      await Permission.contacts.request();
+      await Permission.contacts.request().then((value) {
+        if (value.isGranted) {
+          fetchContacts(); // Fetch contacts after permission is granted
+        }
+      });
     }
   }
 
@@ -53,6 +57,9 @@ class _HomeScreenState extends State<HomeScreen> {
     });
 
     Iterable<Contact> allContacts = await ContactController().getAllContacts();
+    setState(() {
+      allContacts;
+    });
 
     originalContacts = [];
     for (var contact in allContacts) {
@@ -69,11 +76,11 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void filterContacts(String query) {
     List<flutter_contacts.Contact> searchResult =
-        originalContacts.where((contact) {
+    originalContacts.where((contact) {
       final nameMatches =
-          contact.displayName.toLowerCase().contains(query.toLowerCase());
+      contact.displayName.toLowerCase().contains(query.toLowerCase());
       final phoneMatches = contact.phones.any(
-          (phone) => phone.number.toLowerCase().contains(query.toLowerCase()));
+              (phone) => phone.number.toLowerCase().contains(query.toLowerCase()));
       return nameMatches || phoneMatches;
     }).toList();
 
@@ -81,7 +88,7 @@ class _HomeScreenState extends State<HomeScreen> {
       if (query.isEmpty) {
         contactsList = originalContacts
             .where((contact) =>
-                contact.displayName.isNotEmpty && contact.phones.isNotEmpty)
+        contact.displayName.isNotEmpty && contact.phones.isNotEmpty)
             .toList();
       } else {
         contactsList = searchResult;
@@ -131,164 +138,157 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: isLoading
           ? const Center(
-              child: CupertinoActivityIndicator(
-                radius: 12,
-              ),
-            )
-          : FutureBuilder(
-              future: ContactController().getAllContacts(),
-              builder: (c, snap) {
-                return ListView.builder(
-                  itemCount: contactsList.length,
-                  itemBuilder: (context, index) {
-                    final contact = contactsList[index];
-                    return GestureDetector(
-                      child: ExpansionTile(
-                        title: ListTile(
-                          leading: Container(
-                            height: 35,
-                            width: 35,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  blurRadius: 7,
-                                  color: Colors.white.withOpacity(0.4),
-                                  offset: const Offset(-3, -3),
-                                ),
-                                BoxShadow(
-                                  blurRadius: 7,
-                                  color: Colors.black.withOpacity(0.5),
-                                  offset: const Offset(4, 5),
-                                ),
-                              ],
-                              borderRadius: BorderRadius.circular(6),
-                              color: const Color(0xff262626),
-                            ),
-                            child: Text(
-                              contact.displayName.isNotEmpty
-                                  ? contact.displayName[0]
-                                  : "DF",
-                              style: TextStyle(
-                                fontSize: 23,
-                                color: Colors.primaries[
-                                    Random().nextInt(Colors.primaries.length)],
-                                fontFamily: "Poppins",
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          title: Text(
-                            contact.displayName,
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          horizontalTitleGap: 15,
-                        ),
-                        trailing: TextButton(
-                          onPressed: () {
-                            padsController.launchPhone(contact.phones.first.number);
-                          },
-                          child: const Column(
-                            children: <Widget>[
-                              Icon(Icons.call),
-                              Padding(
-                                padding: EdgeInsets.symmetric(vertical: 2.0),
-                              ),
-                              //Text('Call'),
-                            ],
-                          ),
-                        ),
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(10),
-                            alignment: Alignment.center,
-                            child: ButtonBar(
-                              alignment: MainAxisAlignment.spaceBetween,
-                              buttonHeight: 62.0,
-                              buttonMinWidth: 50.0,
-                              children: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    padsController.launchMessage(contact.phones.first.number);
-                                  },
-                                  child: const Column(
-                                    children: <Widget>[
-                                      Icon(Icons.message_outlined),
-                                      Padding(
-                                        padding:
-                                            EdgeInsets.symmetric(vertical: 1.0),
-                                      ),
-                                      Text('Message'),
-                                    ],
-                                  ),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    String phoneNumber =
-                                        contact.phones.first.number;
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => HistoryScreen(
-                                          phoneNumber: phoneNumber,
-                                          userName: contact.displayName,
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                  child: const Column(
-                                    children: <Widget>[
-                                      Icon(Icons.history),
-                                      Padding(
-                                        padding:
-                                            EdgeInsets.symmetric(vertical: 1.0),
-                                      ),
-                                      Text('History'),
-                                    ],
-                                  ),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    padsController.getCallLogs(contact.phones.first.number);
-                                    int indexOfContact =
-                                        index; // Get the index of the current contact
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            UpdatedContactScreen(
-                                          index: indexOfContact,
-                                          id: contact.id,
-                                          contacts: contactsList,
-                                          userImage: contact.photo,
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                  child: const Column(
-                                    children: <Widget>[
-                                      Icon(Icons.update),
-                                      Padding(
-                                        padding:
-                                            EdgeInsets.symmetric(vertical: 1.0),
-                                      ),
-                                      Text("Update"),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
+        child: CupertinoActivityIndicator(
+          radius: 12,
+        ),
+      )
+          : ListView.builder(
+        itemCount: contactsList.length,
+        itemBuilder: (context, index) {
+          final contact = contactsList[index];
+          return GestureDetector(
+            child: ExpansionTile(
+              title: ListTile(
+                leading: Container(
+                  height: 35,
+                  width: 35,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 7,
+                        color: Colors.white.withOpacity(0.4),
+                        offset: const Offset(-3, -3),
                       ),
-                    );
-                  },
-                );
-              }),
+                      BoxShadow(
+                        blurRadius: 7,
+                        color: Colors.black.withOpacity(0.5),
+                        offset: const Offset(4, 5),
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(6),
+                    color: const Color(0xff262626),
+                  ),
+                  child: Text(
+                    contact.displayName.isNotEmpty
+                        ? contact.displayName[0]
+                        : "DF",
+                    style: TextStyle(
+                      fontSize: 23,
+                      color: Colors.primaries[
+                      Random().nextInt(Colors.primaries.length)],
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                title: Text(
+                  contact.displayName,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                horizontalTitleGap: 15,
+              ),
+              trailing: TextButton(
+                onPressed: () {
+                  padsController.launchPhone(contact.phones.first.number);
+                },
+                child: const Column(
+                  children: <Widget>[
+                    Icon(Icons.call),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 2.0),
+                    ),
+                    //Text('Call'),
+                  ],
+                ),
+              ),
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  child: ButtonBar(
+                    alignment: MainAxisAlignment.spaceBetween,
+                    buttonHeight: 62.0,
+                    buttonMinWidth: 50.0,
+                    children: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          padsController.launchMessage(contact.phones.first.number);
+                        },
+                        child: const Column(
+                          children: <Widget>[
+                            Icon(Icons.message_outlined),
+                            Padding(
+                              padding:
+                              EdgeInsets.symmetric(vertical: 1.0),
+                            ),
+                            Text('Message'),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          String phoneNumber = contact.phones.first.number;
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HistoryScreen(
+                                phoneNumber: phoneNumber,
+                                userName: contact.displayName,
+                              ),
+                            ),
+                          );
+                        },
+                        child: const Column(
+                          children: <Widget>[
+                            Icon(Icons.history),
+                            Padding(
+                              padding:
+                              EdgeInsets.symmetric(vertical: 1.0),
+                            ),
+                            Text('History'),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          padsController.getCallLogs(contact.phones.first.number);
+                          int indexOfContact = index; // Get the index of the current contact
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  UpdatedContactScreen(
+                                    index: indexOfContact,
+                                    id: contact.id,
+                                    contacts: contactsList,
+                                    userImage: contact.photo,
+                                  ),
+                            ),
+                          );
+                        },
+                        child: const Column(
+                          children: <Widget>[
+                            Icon(Icons.update),
+                            Padding(
+                              padding:
+                              EdgeInsets.symmetric(vertical: 1.0),
+                            ),
+                            Text("Update"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
-
